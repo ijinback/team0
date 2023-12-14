8de5e5fdd31973f6692f838e18dc69a345a3a49d
mwong20bu <144298179+mwong20bu@users.noreply.github.com>
2023-12-13 21:40:10 -0500

    Updated route for recipes in main.py

--- RecipeFinder/main.py
+++ RecipeFinder/main.py
@@ -12,7 +12,7 @@ SPOON_API_KEY = 'd1dea6315e7941dd9ce6d6924f4f00d0'
 # Target API key 
 TARGET_API_KEY = 'D30EF50D4B06453384B60194C7EEAC5F'
 
-@app.route('/editedrecipes', methods=['GET'])
+@app.route('/recipes', methods=['GET'])
 #@cross_origin()
 def get_recipes():
     ingredients = request.args.get('ingredients')
